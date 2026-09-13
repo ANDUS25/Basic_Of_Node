122e9597418396db88e13ec9dfc24cf122253f2c
ANDUS25 <anandchavan113@gmail.com>
2026-09-13 08:43:18 +0530

    make changes in path module

--- Module/Basic/globalThis.js
+++ Module/Basic/globalThis.js
@@ -1,3 +1,8 @@
 // Like react everythings knows as a Component as in node everyhtings know as "Module"
 
+// We can use this globaThis obejct in every javascript evnironment like node, browser, etc. It is a global object that provides access to the global scope.
+
 globalThis.console.log("Hello Friends");
+
+// like browser we can't use the window object in node because node run on server side. we will get error if we use window object in node. but we can use window object in browser because browser run on client side.
+// window.console.log("Hello Friends");
