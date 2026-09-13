1b9c42ad081a88bcdb34eb74c5ba7900039bedfa
ANDUS25 <anandchavan113@gmail.com>
2026-09-13 08:43:18 +0530

    Make changes in OS module

--- Module/OS/OS.js
+++ Module/OS/OS.js
@@ -1,9 +1,16 @@
-import os from 'os';
+import os from "os";
 
-console.log("Architecture==> ",os.arch());
+console.log("Architecture==> ", os.arch());
 console.log("CPU==> ", os.cpus());
-console.log("version==> ",os.version());
-console.log("Platform==> ",os.platform());
+console.log("version==> ", os.version());
+console.log("Platform==> ", os.platform());
 console.log("homedir==> ", os.homedir());
-console.log("total memory==> ", ((os.totalmem()/1024)/1024)/1024 + " GB");
-console.log("total memory==> ", ((os.freemem()/1024)/1024)/1024 + " GB");
+console.log("total memory==> ", os.totalmem() / 1024 / 1024 / 1024 + " GB");
+console.log("total memory==> ", os.freemem() / 1024 / 1024 / 1024 + " GB");
+console.log("Constants==> ", os.constants);
+console.log("Home Dir==> ", os.homedir());
+console.log("User Info==> ", os.userInfo());
+console.log("Host Name==> ", os.hostname());
+console.log("load average==> ", os.loadavg());
+console.log("Machine==> ", os.machine());
+console.log("Network Interfaces==> ", os.networkInterfaces());
--- Module/Path/basic.js
+++ Module/Path/basic.js
@@ -1,15 +1,28 @@
 // Path is a in-build module in node js, no need to use any external package for path
 
-import { dirname, basename, extname, join, parse, relative, } from "path";
+import {
+  dirname,
+  basename,
+  extname,
+  join,
+  parse,
+  relative,
+  format,
+  isAbsolute,
+} from "path";
 
 console.log("====================================");
 //Those are inbuilt modules which are globally available in Node.js
+// This --dirname and __filename are only availbel in common.js noe in module.js
 // console.log(__filename);
 // console.log(dirname);
 
-const pathName = join("Study","Node","Basic.js")
+// In windows it work with "\" but in mac or linux it work with "/" so because of this use join menthod which handle all secnarios.
+const pathName = join("Study", "Node", "Basic.js");
 
-console.log('fileName',basename(pathName));
+console.log("pathName ", pathName);
+
+console.log("fileName", basename(pathName));
 
 // return file directory location (remove file name)
 console.log("dirName ==>", dirname(pathName));
@@ -19,6 +32,24 @@ console.log("extname ==>", extname(pathName));
 
 // Return one object with information about the path
 console.log("Parse Object==>", parse(pathName));
+
+// Return boolean value if the path is absolute or not. Absolute path means the full path from root directory to the file.
+console.log("Absoulte Path==>", isAbsolute("/Module/Path/basic.js"));
+console.log("Absoulte Path==>", isAbsolute("basic.js"));
+
+console.log("Relative Path==>", relative("E:/study/node", "basic.js"));
+
+// Return the file path related data in object format within details.
+console.log(
+  "Format==>",
+  format({
+    root: "igonred",
+    dir: "E:/study/node",
+    base: "basic.js",
+    name: "igonred",
+    ext: ".js",
+  }),
+);
 
 // Return a path from __ to __
 console.log(relative("E:/study/node", "basic.js"));
